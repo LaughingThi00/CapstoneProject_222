86b2db5b9fd919da14e5018a61176d352b887d17
LaughingThi00 <92248229+LaughingThi00@users.noreply.github.com>
2023-03-29 23:13:56 +0700

    update wallet.js after merge

--- integrate/blockchain/service/wallet.js
+++ integrate/blockchain/service/wallet.js
@@ -25,40 +25,6 @@ async function createWallet (userId) {
         mnemonic: walletEther.mnemonic.phrase
       }
 
-<<<<<<< HEAD
-=======
-    const walletEther = createWalletEther();
-    const bitcoinWallet = createWalletBitcoin()
-    await Wallet.create({
-        userId: userId,
-        key: {
-            bitcoin: {
-                networkId: 1,
-                networkName: "Bitcoin",
-                address: bitcoinWallet.address,
-                publicKey: bitcoinWallet.publicKey,
-                privateKey: bitcoinWallet.privateKey,
-                mnemonic: bitcoinWallet.mnemonic
-            },
-            evm:{
-                networkId: 2,
-                networkName: "EVM",
-                address: walletEther.address,
-                publicKey: walletEther.publicKey,
-                privateKey: walletEther.privateKey,
-                mnemonic: walletEther.mnemonic
-            }
-
-        },
-        created_time: new Date(),
-        updated_time:  new Date(),
-        updated_by: 'core_api',
-    })
-    
-    return {
-        addressBitcoin: bitcoinWallet.address,
-        addressEther: walletEther.address
->>>>>>> 33e38c1561b00aef39056918d7f521ee13ff6509
     }
   })
 
