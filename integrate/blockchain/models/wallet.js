c79eb0601b1f5c8e2f803c5628b76944954701ad
dinhthanh <dinhthanh2000a@gmail.com>
2023-04-07 00:14:34 +0700

    Add deposit jobs

--- integrate/blockchain/models/wallet.js
+++ integrate/blockchain/models/wallet.js
@@ -2,25 +2,28 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const WalletSchema = new Schema({
-  userId: String,
-  key: {
-    evm: {
-      networkId: Number,
-      networkName: String,
-      address: String,
-      publicKey: String,
-      privateKey: String,
-      mnemonic: String
+	userId: String,  
+    key: {
+        evm: {
+            networkId: Number,
+            networkName: String,
+            address: String,
+            publicKey: String,
+            privateKey: String,
+            mnemonic: String,
+        },
+        bitcoin:{
+            networkId: Number,
+            networkName: String,
+            address: String,
+            publicKey: String,
+            privateKey: String,
+            mnemonic: String,
+        }
     },
-    bitcoin: {
-      networkId: Number,
-      networkName: String,
-      address: String,
-      publicKey: String,
-      privateKey: String,
-      mnemonic: String
-    }
-  }
+    created_time: Date,
+    updated_time: Date,
+    updated_by: String
 
 })
 
--- integrate/blockchain/utils/contract.js
+++ integrate/blockchain/utils/contract.js
@@ -1,42 +1,32 @@
-const { Web3 } = require('./web3')
-const fs = require('fs')
-const path = require('path')
+const Web3 = require('./web3');
+const fs = require('fs');
+const path = require("path");
 
 class Contract {
-  static getContract ({
-    abi,
-    chainId,
-    address,
-    withRandom
-  }) {
-    let web3 = Web3.httpProvider(chainId)
-    if (withRandom) {
-      web3 = Web.randomProvider(chainId)
+    static getContract({
+        abi,
+        chainId,
+        address,
+      }) {
+        let web3 = Web3.httpProvider(chainId)
+        const contract = new web3.eth.Contract(abi, address);
+    
+        return contract;
+      }
+    static getTokenContract({chainId,address}){
+        let tokenABIPath = path.join(__dirname, '../constants/abis/ERC20.json')
+        let tokenABI = fs.readFileSync(tokenABIPath, {
+            encoding: 'utf-8'
+        });
+        let loadTokenABI = JSON.parse(tokenABI);
+        const contract = Contract.getContract({
+            abi: loadTokenABI,
+            chainId,
+            address,
+        });
+
+        return contract;
     }
-    // providerType == ProviderType.WS
-    //   ? Web3.wsProvider(chainId)
-    //   : Web3.httpProvider(chainId);
-
-    const contract = new web3.eth.Contract(abi, address)
-
-    return contract
-  }
-
-  static getTokenContract ({
-    chainId,
-    address
-  }) {
-    const tokenABIPath = path.join(__dirname, '../constants/abis/ERC20.json')
-    const tokenABI = fs.readFileSync(tokenABIPath, {
-      encoding: 'utf-8'
-    })
-    const loadTokenABI = JSON.parse(tokenABI)
-    const contract = Contract.getContract({
-      abi: loadTokenABI,
-      chainId,
-      address
-    })
-
-    return contract
-  }
 }
+
+module.exports = Contract
